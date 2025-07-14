48279bba5d3eed232c9815751e1b8bbd2a7c2c32
Craig McLean <craig@candi-1.lan.candi-home.com>
2025-07-14 12:35:25 +0000

    updated build/compose and added template env file

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 docker build \
   --build-arg GIT_COMMIT=$(git rev-parse HEAD) \
   --build-arg GIT_VERSION=$(git describe --tags --always --dirty 2>/dev/null || git rev-parse --short HEAD) \
-  -t vuln .
+  -t mcleancraig/vuln-app .
