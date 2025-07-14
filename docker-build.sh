21fdb2fe55befc02914b3863fc215959e7a41f32
Craig McLean <craig.mclean@bisimulations.com>
2025-07-14 15:10:20 +0100

    add version to docker build tags

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 docker build \
   --build-arg GIT_COMMIT=$(git rev-parse HEAD) \
   --build-arg GIT_VERSION=$(git describe --tags --always --dirty 2>/dev/null || git rev-parse --short HEAD) \
-  -t mcleancraig/vuln-app .
+  -t mcleancraig/vuln-app:${GIT_VERSION} .
